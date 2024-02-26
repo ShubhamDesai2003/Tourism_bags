513a0f88543e32aaf11f5a410d98f2a8ed2425e3
Shubham <92636412+ShubhamDesai2003@users.noreply.github.com>
2024-02-26 08:24:12 +0530

    database

--- database/bag_db.sql
+++ database/bag_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 21, 2024 at 06:23 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 26, 2024 at 02:52 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,9 +27,11 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-CREATE TABLE `admin` (
-  `name` varchar(20) NOT NULL,
-  `pass` varchar(40) NOT NULL
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `name` varchar(20) COLLATE latin1_general_ci NOT NULL,
+  `pass` varchar(40) COLLATE latin1_general_ci NOT NULL,
+  PRIMARY KEY (`name`,`pass`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
@@ -37,54 +39,35 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`name`, `pass`) VALUES
-('admin', 'admin');
+('admin', 'admin'),
+('shubham', '123');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `books`
+-- Table structure for table `bags`
 --
 
-CREATE TABLE `books` (
-  `book_id` int(11) NOT NULL,
-  `title` varchar(60) DEFAULT NULL,
-  `image` varchar(40) DEFAULT NULL,
+DROP TABLE IF EXISTS `bags`;
+CREATE TABLE IF NOT EXISTS `bags` (
+  `book_id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(60) COLLATE latin1_general_ci DEFAULT NULL,
+  `image` varchar(40) COLLATE latin1_general_ci DEFAULT NULL,
   `price` decimal(6,2) NOT NULL,
-  `quantity` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+  `quantity` int UNSIGNED NOT NULL,
+  PRIMARY KEY (`book_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
--- Dumping data for table `books`
+-- Dumping data for table `bags`
 --
 
-INSERT INTO `books` (`book_id`, `title`, `image`, `price`, `quantity`) VALUES
-(1, 'Learning Mobile App Development', 'bag01.jpg', 20.00, 6),
-(2, 'Doing Good By Doing Good', 'bag01.jpg', 20.00, 2),
-(3, 'shubham', 'bag01.jpg', 0.00, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `customers`
---
-
-CREATE TABLE `customers` (
-  `customerid` int(10) UNSIGNED NOT NULL,
-  `name` varchar(60) NOT NULL,
-  `address` varchar(80) NOT NULL,
-  `city` varchar(30) NOT NULL,
-  `zip_code` varchar(10) NOT NULL,
-  `country` varchar(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-
---
--- Dumping data for table `customers`
---
-
-INSERT INTO `customers` (`customerid`, `name`, `address`, `city`, `zip_code`, `country`) VALUES
-(1, 'a', 'a', 'a', 'a', 'a'),
-(2, 'b', 'b', 'b', 'b', 'b'),
-(3, 'test', '123 test', '12121', 'test', 'test');
+INSERT INTO `bags` (`book_id`, `title`, `image`, `price`, `quantity`) VALUES
+(1, 'School Bag', 'image3.webp', '20.00', 20),
+(2, 'College Bag', 'bag01.jpg', '20.00', 19),
+(3, 'Tourist Bag', 'bag01.jpg', '20.00', 20),
+(8, 'School Bag', './image', '20.00', 20),
+(9, 'School Bag', './image', '20.00', 20);
 
 -- --------------------------------------------------------
 
@@ -92,54 +75,23 @@ INSERT INTO `customers` (`customerid`, `name`, `address`, `city`, `zip_code`, `c
 -- Table structure for table `orders`
 --
 
-CREATE TABLE `orders` (
-  `orderid` int(10) UNSIGNED NOT NULL,
-  `customerid` int(10) UNSIGNED NOT NULL,
-  `amount` decimal(6,2) DEFAULT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `ship_name` char(60) NOT NULL,
-  `ship_address` char(80) NOT NULL,
-  `ship_city` char(30) NOT NULL,
-  `ship_zip_code` char(10) NOT NULL,
-  `ship_country` char(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `order_id` int UNSIGNED NOT NULL,
+  `name` varchar(20) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`orderid`, `customerid`, `amount`, `date`, `ship_name`, `ship_address`, `ship_city`, `ship_zip_code`, `ship_country`) VALUES
-(1, 1, 60.00, '2015-12-03 13:30:12', 'a', 'a', 'a', 'a', 'a'),
-(2, 2, 60.00, '2015-12-03 13:31:12', 'b', 'b', 'b', 'b', 'b'),
-(3, 3, 20.00, '2015-12-03 19:34:21', 'test', '123 test', '12121', 'test', 'test'),
-(4, 1, 20.00, '2015-12-04 10:19:14', 'a', 'a', 'a', 'a', 'a');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `order_items`
---
-
-CREATE TABLE `order_items` (
-  `orderid` int(10) UNSIGNED NOT NULL,
-  `book_isbn` varchar(20) NOT NULL,
-  `item_price` decimal(6,2) NOT NULL,
-  `quantity` tinyint(3) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-
---
--- Dumping data for table `order_items`
---
-
-INSERT INTO `order_items` (`orderid`, `book_isbn`, `item_price`, `quantity`) VALUES
-(1, '978-1-118-94924-5', 20.00, 1),
-(1, '978-1-44937-019-0', 20.00, 1),
-(1, '978-1-49192-706-9', 20.00, 1),
-(2, '978-1-118-94924-5', 20.00, 1),
-(2, '978-1-44937-019-0', 20.00, 1),
-(2, '978-1-49192-706-9', 20.00, 1),
-(3, '978-0-321-94786-4', 20.00, 1),
-(1, '978-1-49192-706-9', 20.00, 1);
+INSERT INTO `orders` (`id`, `order_id`, `name`, `date`) VALUES
+(12, 1, 'Learning Mobile App ', '2024-02-25 16:39:04'),
+(13, 2, 'Doing Good By Doing ', '2024-02-25 16:52:56'),
+(14, 2, 'College Bag', '2024-02-25 17:27:03');
 
 -- --------------------------------------------------------
 
@@ -147,98 +99,28 @@ INSERT INTO `order_items` (`orderid`, `book_isbn`, `item_price`, `quantity`) VAL
 -- Table structure for table `personal_info`
 --
 
-CREATE TABLE `personal_info` (
-  `ID` int(23) NOT NULL,
-  `Dish` varchar(255) NOT NULL,
-  `Price` varchar(255) NOT NULL,
-  `Quantity` int(255) NOT NULL,
-  `address` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `publisher`
---
-
-CREATE TABLE `publisher` (
-  `publisherid` int(10) UNSIGNED NOT NULL,
-  `publisher_name` varchar(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+DROP TABLE IF EXISTS `personal_info`;
+CREATE TABLE IF NOT EXISTS `personal_info` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `phone` int NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `publisher`
+-- Dumping data for table `personal_info`
 --
 
-INSERT INTO `publisher` (`publisherid`, `publisher_name`) VALUES
-(1, 'Wrox'),
-(2, 'Wiley'),
-(3, 'O\'Reilly Media'),
-(4, 'Apress'),
-(5, 'Packt Publishing'),
-(6, 'Addison-Wesley');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`name`,`pass`);
-
---
--- Indexes for table `books`
---
-ALTER TABLE `books`
-  ADD PRIMARY KEY (`book_id`);
-
---
--- Indexes for table `customers`
---
-ALTER TABLE `customers`
-  ADD PRIMARY KEY (`customerid`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`orderid`);
-
---
--- Indexes for table `publisher`
---
-ALTER TABLE `publisher`
-  ADD PRIMARY KEY (`publisherid`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `books`
---
-ALTER TABLE `books`
-  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `customers`
---
-ALTER TABLE `customers`
-  MODIFY `customerid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `orderid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `publisher`
---
-ALTER TABLE `publisher`
-  MODIFY `publisherid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+INSERT INTO `personal_info` (`ID`, `name`, `email`, `phone`, `address`) VALUES
+(26, 'Shubham Desai', 'shubhamdesai20003@gmail.com', 7655665, 'ss'),
+(27, 'Shubham Desai', 'shubhamdesai20003@gmail.com', 7655665, 'ss'),
+(28, 'Shubham Desai', 'shubhamdesai20003@gmail.com', 7655665, 'ss'),
+(29, 'Shubham Desai', 'shubhamdesai20003@gmail.com', 7655665, 'ss'),
+(30, 'Shubham Desai', 'shubhamdesai20003@gmail.com', 7655665, 'ss'),
+(31, 'Shubham Desai', 'shubhamdesai20003@gmail.com', 7655665, 'ss'),
+(32, 'Shubham Desai', 'shubhamdesai20003@gmail.com', 7655665, 'ss');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
